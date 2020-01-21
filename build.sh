d3a202656f6401f5a1d79e5dc3447fc972487617
Manel Montilla <manel.montilla@schibsted.com>
2020-01-21 16:58:22 +0100

    Use go modules

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+# Ensure the current version of gogen is installed
+
+go install github.com/goadesign/goa/goagen
+
+
 # Autogenerate content
 goagen bootstrap -d github.com/adevinta/vulcan-results/design
 
